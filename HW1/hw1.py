9cc33e235f2630647c704c95362d1577df27de66
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-09-21 13:21:27 +0800

    Doing Fintech HW1

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[86]:
+# In[87]:
 
 
 import pandas as pd
 import numpy
 import csv
 import sys, os
-import warnings
-warnings.filterwarnings('ignore')
 
 input_day = sys.argv[1]
 input_day = input_day.replace('Daily_', '')
 input_day = input_day.replace('_', '')
 input_day = input_day.replace('.csv', '')
 
-df_orig = pd.read_csv('Daily_2019_09_20.csv', encoding = 'big5')
-#print(df.columns)
+df_orig = pd.read_csv(sys.argv[1], encoding = 'big5')
 df = df_orig[df_orig['商品代號'] == 'TX     ']
 df = df[df['成交日期'] == int(input_day)]
 print(df_orig.iloc[[df['成交時間'].argmin()]]['成交價格'].values,      df_orig.iloc[[df['成交價格'].argmax()]]['成交價格'].values,      df_orig.iloc[[df['成交價格'].argmin()]]['成交價格'].values,      df_orig.iloc[[df['成交時間'].argmax()]]['成交價格'].values, )
