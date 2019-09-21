74f0c288f145d3bdc323216285b6a22301dc27c8
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-09-21 13:33:14 +0800

    Doing Fintech HW1

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,11 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[87]:
-
-
-import pandas as pd
 import numpy
+import pandas as pd
 import csv
 import sys, os
 
@@ -17,5 +11,5 @@ input_day = input_day.replace('.csv', '')
 df_orig = pd.read_csv(sys.argv[1], encoding = 'big5')
 df = df_orig[df_orig['商品代號'] == 'TX     ']
 df = df[df['成交日期'] == int(input_day)]
-print(df_orig.iloc[[df['成交時間'].argmin()]]['成交價格'].values,      df_orig.iloc[[df['成交價格'].argmax()]]['成交價格'].values,      df_orig.iloc[[df['成交價格'].argmin()]]['成交價格'].values,      df_orig.iloc[[df['成交時間'].argmax()]]['成交價格'].values, )
+print(df_orig.iloc[[df['成交時間'].argmin()]]['成交價格'].values, df_orig.iloc[[df['成交價格'].argmax()]]['成交價格'].values, df_orig.iloc[[df['成交價格'].argmin()]]['成交價格'].values, df_orig.iloc[[df['成交時間'].argmax()]]['成交價格'].values)
 
