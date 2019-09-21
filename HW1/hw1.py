2990213294bb6b2fb6b6d2f34502e9b66c8f477c
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-09-21 17:54:54 +0800

    Doing Fintech HW1

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,15 +1,28 @@
-import numpy
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
 import pandas as pd
+import numpy
 import csv
 import sys, os
 
-input_day = sys.argv[1]
-input_day = input_day.replace('Daily_', '')
-input_day = input_day.replace('_', '')
-input_day = input_day.replace('.csv', '')
-
-df_orig = pd.read_csv(sys.argv[1], encoding = 'big5')
+df_orig = pd.read_csv('Daily_2018_10_01.csv', encoding = 'big5')
 df = df_orig[df_orig['商品代號'] == 'TX     ']
-df = df[df['成交日期'] == int(input_day)]
-print(df_orig.iloc[[df['成交時間'].argmin()]]['成交價格'].values, df_orig.iloc[[df['成交價格'].argmax()]]['成交價格'].values, df_orig.iloc[[df['成交價格'].argmin()]]['成交價格'].values, df_orig.iloc[[df['成交時間'].argmax()]]['成交價格'].values)
+#print(df.columns)
+focus_day = df.iloc[[0]]['成交日期']
+focus_month = df.iloc[[0]]['到期月份(週別)']
+data_all = df.values.tolist()
+data_parsed = []
+for each_row in data_all:
+    if('/' in each_row[2]):
+        continue #ignore entry
+        
+    if int(each_row[0]) == int(focus_day.values)     and int(each_row[2]) == int(focus_month.values)     and int(each_row[3]) >= 84500     and int(each_row[3]) <= 134500:
+        print('append -> ', each_row)
+        data_parsed.append(float(each_row[4]))
+        
+print(int(data_parsed[0]), int(max(data_parsed)), int(min(data_parsed)), int(data_parsed[-1]))
 
