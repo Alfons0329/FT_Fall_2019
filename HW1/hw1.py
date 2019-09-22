2a5c2639c6b2ea82bff2f0443d37a1569f47c392
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-09-22 13:50:48 +0800

    Different version

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -3,8 +3,37 @@
 # In[21]:
 import csv
 import sys
+from datetime import datetime, timedelta
 
 parsed_data = []
+def nth_weekday(date, week, day):
+    tmp = date.replace(day = 1)
+    adj = (day - tmp.weekday()) % 7
+    tmp += timedelta(days = adj)
+    tmp += timedelta(weeks = week - 1)
+    return tmp
+
+def is_in_month(date):
+    datetime_obj = datetime.strptime(date, '%Y%m%d')
+    cmp_obj = nth_weekday(datetime(datetime_obj.year, datetime_obj.month, datetime_obj.day), 3, 2)
+
+    focus_month = ''
+    if cmp_obj.day < datetime_obj.day: # go next month
+        if cmp_obj.month < 9:
+            focus_month = str(cmp_obj.year) + '0' + str(int(cmp_obj.month) + 1)
+        elif cmp_obj.month == 12:
+            focus_month = str(int(cmp_obj.year) + 1) + str(int(cmp_obj.month) + 1)
+        else:
+            focus_month = str(cmp_obj.year) + str(int(cmp_obj.month) + 1)
+    else: # in this month
+        if cmp_obj.month <= 9:
+            focus_month = str(cmp_obj.year) + '0' + str(int(cmp_obj.month))
+        else:
+            focus_month = str(cmp_obj.year) + str(int(cmp_obj.month))
+
+    print(date, datetime_obj.day, datetime_obj.month, 'focus month ', focus_month)
+    return focus_month
+
 with open(sys.argv[1], encoding = 'big5') as f:
     all_data = csv.reader(f)
     row_idx = 0
@@ -14,10 +43,12 @@ with open(sys.argv[1], encoding = 'big5') as f:
     for each_row in all_data:
         if row_idx:
             if str(each_row[1][0:2]) == 'TX':
+                #print('len ', len(each_row[2][0:6]), ' ', len(focus_month[0:6]))
+                print('row2', each_row[2][0:6], '|', focus_month[0:6], '|')
                 if flg == 0 and '/' not in each_row[2] and int(each_row[3]) == 84500:
-                    focus_month = each_row[2]
+                    focus_month = is_in_month(each_row[0])
                     flg = 1
-                if int(each_row[3]) >= 84500 and int(each_row[3]) <= 134500 and each_row[2] == focus_month:
+                if int(each_row[3]) >= 84500 and int(each_row[3]) <= 134500 and each_row[2][0:6] == focus_month[0:6]:
                     parsed_data.append(int(each_row[4]))
 
         row_idx += 1
