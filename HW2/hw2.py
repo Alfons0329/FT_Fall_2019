1d13f59d748384bc7635515dded14be4b3be5a34
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-10-10 22:26:06 +0800

    Tuning and optimizing

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -1,6 +1,6 @@
 '''
 Work progress:
-1010: Design the structure, using weighted moving average(weighted), checkCSV
+1010: Design the structure, using weighted moving average(weighted), check csv file
 '''
 
 '''
@@ -14,14 +14,6 @@ import os, sys, csv
 import argparse
 
 # implementation part of HW2
-## for brute force search argument parsing
-'''
-parser.add_argument('--alpha', type = int, 10)
-parser.add_argument('--beta', type = int, 10)
-parser.add_argument('--windows_size', type = int, 290)
-args = parser.parse_args()
-'''
-
 # param limits
 WIN_LOWER = 200
 WIN_UPPER = 300 + 1
@@ -39,6 +31,7 @@ def myStrategy(pastData, currPrice, stockType, w, a, b):
     weighted_sum = 0
     weighted_len = 0
     weighted_ma  = 0
+    
     for i in range(0, len(windowed_data)):
         weighted_sum += windowed_data[i] * i
         weighted_len += i
@@ -47,75 +40,77 @@ def myStrategy(pastData, currPrice, stockType, w, a, b):
     action = 0
     if currPrice - a > weighted_ma:
         action = 1
-    if currPrice + b < weighted_ma:
+    elif currPrice + b < weighted_ma:
         action = -1
     else:
         action = 0
 
     return action
 
-def calculateReturnRate(file, stocksType, w, a, b):
-    # stocksType = "SPY" or "IAU" or "DSI" , "LQD"
-    # read file
-    file = 'SPY.csv'
-    df = pd.read_csv(file)
-    adjClose = df["Adj Close"].values   # get adj close
-    dataCount=len(adjClose) # day size
-
-    # init.
-    capital = 1  # 持有資金
-    capitalOrig = capital  # cost
-    suggestedAction = np.zeros((dataCount,1))  # 判斷action
-    stockHolding = np.zeros((dataCount,1))  # 持有股票
-    total = np.zeros((dataCount,1))  # 結算資金
-    realAction = np.zeros((dataCount,1))  # 實際action
-
-    # run each day
+# compute the return rate of my strategy, this code is from TA
+def computeReturnRate(priceVec, stockType, w, a, b):
+    capital=1000    # Initial available capital
+    capitalOrig=capital     # original capital
+    dataCount=len(priceVec)                # day size
+    suggestedAction=np.zeros((dataCount,1))    # Vec of suggested actions    stockHolding=np.zeros((dataCount,1))      # Vec of stock holdings
+    total=np.zeros((dataCount,1))         # Vec of total asset
+    stockHolding=np.zeros((dataCount,1))  # Vec of stock holdings
+    realAction=np.zeros((dataCount,1))    # Real action, which might be different from suggested action. For instance, when the suggested action is 1 (buy) but you don't have any capital, then the real action is 0 (hold, or do nothing). 
+    
+    # Run through each day
     for ic in range(dataCount):
-        currPrice = adjClose[ic]  # 當天價格
-        suggestedAction[ic] = myStrategy(adjClose[0:ic], currPrice, stocksType, w, a, b) # 取得當天action
-
+        currentPrice=priceVec[ic]    # current price
+        suggestedAction[ic]=myStrategy(priceVec[0:ic], currentPrice, stockType, w, a, b)        # Obtain the suggested action
+        
         # get real action by suggested action
-        if ic > 0:
-            # 更新手上持有股票
-            stockHolding[ic] = stockHolding[ic-1]
-        if suggestedAction[ic] == 1:
-            # 若未持有股票: 買
-            if stockHolding[ic] == 0:
-                stockHolding[ic] = capital/currPrice # 買入股票
-                capital = 0   # 持有資金
-                realAction[ic] = 1
-        elif suggestedAction[ic] == -1:
-            # 若持有股票： 賣
-            if stockHolding[ic] > 0:
-                capital = stockHolding[ic] * currPrice # 賣出股票
-                stockHolding[ic] = 0  # 持有股票
-                realAction[ic] = -1
-        elif suggestedAction[ic] == 0:
-            # 不買不賣
+        if ic>0:
+            stockHolding[ic]=stockHolding[ic-1]    # The stock holding from the previous day
+        if suggestedAction[ic]==1:    # Suggested action is "buy"
+            if stockHolding[ic]==0:        # "buy" only if you don't have stock holding
+                stockHolding[ic]=capital/currentPrice # Buy stock using cash
+                capital=0    # Cash
+                realAction[ic]=1
+        elif suggestedAction[ic]==-1:    # Suggested action is "sell"
+            if stockHolding[ic]>0:        # "sell" only if you have stock holding
+                capital=stockHolding[ic]*currentPrice # Sell stock to have cash
+                stockHolding[ic]=0    # Stocking holding
+                realAction[ic]=-1
+        elif suggestedAction[ic]==0:    # No action
             realAction[ic]=0
         else:
             assert False
-        # 當天結算資金
-        total[ic] = capital + stockHolding[ic] * currPrice
-    # 最終盈利率
-    returnRate = (total[-1] - capitalOrig) / capitalOrig
+        total[ic]=capital+stockHolding[ic]*currentPrice    # Total asset, including stock holding and cash 
+    returnRate=(total[-1]-capitalOrig)/capitalOrig        # Return rate of this run
+    
     return returnRate
-
-if __name__ == '__main__':
-
+    
+if __name__=='__main__':
+    file=sys.argv[1]    # input file
+    file = 'SPY.csv'
+    df=pd.read_csv(file)    
+    adjClose=df["Adj Close"].values    # Get adj close as the price vector
+    stockType=file[-7:-4]        # Get stock type
+    bestReturnRate = 0
+    
+    w_best = 0
+    a_best = 0
+    b_best = 0
+    
     for w in range(WIN_LOWER, WIN_UPPER, 5):
         for a in range(0, ALPHA_MAX):
             for b in range(0, BETA_MAX):
-                #print('strategy win %d alpha %d beta %d '%(w, a, b))
-                returnRate = calculateReturnRate(sys.argv[1], sys.argv[1][-7 : -4], w, a, b) # (file , stock_name)
-                result_str = str(w) + ',' + str(a) + ',' + str(b) + ',' + str(returnRate) + '\n'
-                '''
-                with open('1010compare.csv', 'a') as f:
-                    f.write(result_str)
-                    f.close()
-                '''
+                
+                returnRate = computeReturnRate(adjClose, stockType, w, a, b)    # Compute return rate
+                
+                if returnRate > bestReturnRate:
+                    bestReturnRate = returnRate
+                    w_best = w
+                    a_best = a
+                    b_best = b
+                    print(w_best,',',a_best,',',b_best,',',bestReturnRate * 10000)
 
-                print(w,',',a,',',b,',',returnRate * 10000)
-
-
+    result_str = str(w) + ',' + str(a) + ',' + str(b) + ',' + str(returnRate) + '\n'
+    print(w_best,',',a_best,',',b_best,',',bestReturnRate * 10000)
+    with open('1010weighted_ma.csv', 'a') as f:
+        f.write(result_str)
+        f.close()
