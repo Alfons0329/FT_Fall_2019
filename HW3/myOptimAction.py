ac04e2d106acd43432b04758c4fd2eefe7a8488f
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-11-17 00:16:27 +0800

    Doing FT HW3, DP buy and sell stock

--- HW3/myOptimAction.py
+++ HW3/myOptimAction.py
@@ -5,7 +5,7 @@ def myOptimAction(priceMat, transFeeRate):
     days, types = priceMat.shape
     max_stock = 0
     max_money = 0
-
+    action_matrix = np.zeros((days, 1), dtype=int)
 
     for i in range(days):
         # try sell to find the best stock selling
