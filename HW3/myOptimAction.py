725d791647b9c63f9baac839b13e34dc71e91082
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-11-17 20:28:00 +0800

    DP not OK, stuck on local optimal with greedy

--- HW3/myOptimAction.py
+++ HW3/myOptimAction.py
@@ -15,17 +15,16 @@ def myOptimAction(priceMat, transFeeRate):
             cash_have *= (1 - transFeeRate)
             dp_record[0] = [cash_have / pa, cash_have / pb, cash_have / pc, cash_have / pd, cash_original]
         else:
-            # sell stock
-            cash_have = dp_record[i - 1][-1]
+            # sell stock, update cash
+            cash_today = 0 
             for j in range(0, 4):
-                if dp_record[i - 1][j] * priceMat[i][j] > cash_have:
-                    cash_have = dp_record[i - 1][j] * priceMat[i][j]
+                if dp_record[i - 1][j] * priceMat[i][j] > cash_today:
+                    cash_today = dp_record[i - 1][j] * priceMat[i][j] * (1 - transFeeRate)
+            dp_record[i][-1] = max(dp_record[i - 1][-1], cash_today)
 
-            # update stock holdings
-            dp_record[i][-1] = cash_have
-            cash_have *= (1 - transFeeRate)
+            # buy stock, update stock
             for j in range(0, 4):
-                dp_record[i][j] = max(dp_record[i - 1][j], cash_have / priceMat[i][j])
-
+                dp_record[i][j] = max(dp_record[i - 1][j], dp_record[i - 1][-1] / priceMat[i][j] * (1 - transFeeRate))
+            
     print(dp_record)
     return action_matrix[::-1]
