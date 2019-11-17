3b68a22dd531da5c6611b0464493a8c846492bf7
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-11-17 22:06:07 +0800

    DP not OK, stuck on local optimal with greedy

--- HW3/myOptimAction.py
+++ HW3/myOptimAction.py
@@ -9,22 +9,24 @@ def myOptimAction(priceMat, transFeeRate):
     # Use DP to find best possibilities
     for i in range(days):
         # init
+        #input()
+        print('-----------')
         pa, pb, pc, pd = priceMat[i]
         if i == 0:
-            cash_original = cash_have
-            cash_have *= (1 - transFeeRate)
-            dp_record[0] = [cash_have / pa, cash_have / pb, cash_have / pc, cash_have / pd, cash_original]
+            dp_record[0] = [cash_have / pa * (1 - transFeeRate), cash_have / pb * (1 - transFeeRate), cash_have / pc * (1 - transFeeRate), cash_have / pd * (1 - transFeeRate), cash_have]
         else:
             # sell stock, update cash
-            cash_today = 0 
             for j in range(0, 4):
-                if dp_record[i - 1][j] * priceMat[i][j] > cash_today:
-                    cash_today = dp_record[i - 1][j] * priceMat[i][j] * (1 - transFeeRate)
-            dp_record[i][-1] = max(dp_record[i - 1][-1], cash_today)
+                if dp_record[i - 1][j] * priceMat[i][j] * (1 - transFeeRate) > cash_have:
+                    print('cash)have %f calculate %f' % (cash_have, dp_record[i - 1][j] * priceMat[i][j] * (1 - transFeeRate)))
+                    cash_have = dp_record[i - 1][j] * priceMat[i][j] * (1 - transFeeRate)
+            dp_record[i][-1] = max(dp_record[i - 1][-1], cash_have)
 
             # buy stock, update stock
             for j in range(0, 4):
                 dp_record[i][j] = max(dp_record[i - 1][j], dp_record[i - 1][-1] / priceMat[i][j] * (1 - transFeeRate))
             
+        print(priceMat[i])
+        print(i, dp_record[i])
     print(dp_record)
     return action_matrix[::-1]
