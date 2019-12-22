420bcfaff92872abdffdaf32f28ce9664a20f00f
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-12-22 11:40:33 +0800

    Try multithread 2

---
+++ Final_Project/main_multithread_2.py
@@ -0,0 +1,28 @@
+import os, sys, threading, time
+
+THREAD_CNT = int(sys.argv[1])
+
+min_search = 100
+max_search = 120
+scale = (max_search - min_search) // THREAD_CNT
+
+class mythread(threading.Thread):
+    def __init__(self, num):
+        threading.Thread.__init__(self)
+        self.num = num
+
+    def run(self):
+        cnt = self.num
+        exe = 'python3 main.py ' + str(min_search + cnt * scale) + ' ' + str(min_search + (cnt + 1) * scale)
+        os.system(exe)
+        print('thread %d finished'%(cnt))
+
+threads = []
+for i in range(THREAD_CNT):
+    threads.append(mythread(i))
+    threads[i].start()
+
+for i in range(THREAD_CNT):
+    threads[i].join()
+
+print('finished all')
