7c5ba332c47e5bf6797b2218c27afaa9ceaac525
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-12-22 14:21:43 +0800

    Added mystrategy

---
+++ Final_Project/myStrategy.py
@@ -0,0 +1,44 @@
+def myStrategy(daily, minutely, opv):
+    l = 49
+    s = 47
+
+    act = 0
+
+    daily = daily['close'].values
+    data_len = len(daily)
+
+    w_l = daily[-l:]
+    w_s = daily[-s:]
+
+    up = 0
+    down = 0
+    for i in range(l - 1):
+        if w_l[i] < w_l[i + 1]:
+            up += (w_l[i + 1] - w_l[i])
+        elif w_l[i] > w_l[i + 1]:
+            down += (w_l[i] - w_l[i + 1])
+
+    if up + down == 0:
+        up += 1
+    rsi_l = float((up) / (up + down))
+
+    up = down = 0
+
+    for i in range(s - 1):
+        if w_s[i] < w_s[i + 1]:
+            up += (w_s[i + 1] - w_s[i])
+        elif w_s[i] > w_s[i + 1]:
+            down += (w_s[i] - w_s[i + 1])
+
+    if up + down == 0:
+        up += 1
+    rsi_s = float((up) / (up + down))
+
+    if rsi_s > rsi_l or rsi_s > 0.5 :
+        act = 1
+    elif rsi_s < rsi_l or rsi_s < 0.5:
+        act = -1
+    else:
+        act = 0
+
+    return act
